486fbcc7d3420ef94d01cf0b7e24e76912a243bc
Joro Protopopov <gopetoo98@gmail.com>
2024-10-04 11:03:34 +0300

    Product admin CRUD functionality upload.

---
+++ src/Form/ProductType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Product;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Count;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('price')
+            ->add('description')
+            ->add('stockQuantity')
+
+            ->add('categories', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'label'=> 'Categories',
+                'attr' => ['class' => 'form-control'],
+                'constraints' => [
+                    new Count([
+                        'min' => 1,
+                        'minMessage' => 'Please select at least one category.',
+                    ]),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
