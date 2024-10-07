2eb7fcf533f964d1aa8b56afe42b4d89af3ee3d6
Vasilen K <vasilen.iliev@rewardgateway.com>
2024-10-07 13:27:01 +0300

    Added a Security Centre for the User where he will be able to amend their email and password. Also added minimum requirements for password

--- src/Form/SecurityCentreType.php
+++ src/Form/SecurityCentreType.php
@@ -3,15 +3,15 @@
 namespace App\Form;
 
 use App\Entity\User;
-use App\Entity\Address;
-use App\Form\AddressFormType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class SecurityCentreType extends AbstractType
 {
@@ -23,6 +23,21 @@ class SecurityCentreType extends AbstractType
                 'type' => PasswordType::class,
                 'first_options' => [
                     'label' => 'Password',
+                    'constraints' => [
+                        new NotBlank(),
+                        new Length([
+                            'min' => 8,
+                            'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        ]),
+                        new Regex([
+                            'pattern' => '/\d/',
+                            'message' => 'Your password should contain at least one number',
+                        ]),
+                        new Regex([
+                            'pattern' => '/[a-zA-Z]/',
+                            'message' => 'Your password should contain at least one letter',
+                        ]),
+                    ],
                 ],
                 'second_options' => [
                     'label' => 'Confirm Password',
@@ -36,7 +51,6 @@ class SecurityCentreType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => User::class,
-            'addresses' => [],
         ]);
     }
 }
