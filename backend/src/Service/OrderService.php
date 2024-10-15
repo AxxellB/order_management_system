e995bcbd42b7712e3cf921a57b989d4045594a6a
Vasilen K <vasilen.iliev@rewardgateway.com>
2024-10-15 10:18:40 +0300

    TestCommit

--- backend/src/Service/OrderService.php
+++ backend/src/Service/OrderService.php
@@ -1,12 +1,14 @@
 <?php
 namespace App\Service;
 
+use App\Controller\OrderController;
 use App\Entity\Address;
 use App\Entity\Order;
 use App\Entity\OrderProduct;
 use App\Enum\OrderStatus;
 use App\Repository\BasketRepository;
 use App\Repository\OrderRepository;
+use App\Repository\ProductRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
@@ -14,10 +16,11 @@ class OrderService
 {
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
-        private readonly UserRepository $userRepository,
-        private readonly OrderRepository $orderRepository,
-        private readonly BasketRepository $basketRepository,
-        private readonly BasketService $basketService
+        private readonly UserRepository         $userRepository,
+        private readonly OrderRepository        $orderRepository,
+        private readonly BasketRepository       $basketRepository,
+        private readonly BasketService          $basketService,
+        private readonly ProductRepository $productRepository
     ) {
     }
 
@@ -63,6 +66,75 @@ class OrderService
         $this->basketService->clearBasket($basket);
         $this->entityManager->persist($basket);
         $this->entityManager->flush();
+        return $order;
+    }
+
+    public function editOrder(int $orderId, array $orderProducts, array $orderAddress): Order
+    {
+        $order = $this->orderRepository->find($orderId);
+        if (!$order) {
+            throw new \Exception('Order not found');
+        }
+
+        if (!in_array($order->getStatus(), [OrderStatus::NEW, OrderStatus::PROCESSING])) {
+            throw new \Exception('This order cannot be modified at this stage');
+        }
+
+        $currentOrderProducts = $order->getOrderProducts();
+        $totalAmount = 0;
+
+        foreach ($orderProducts as $productId => $quantity) {
+            $product = $this->productRepository->find($productId);
+            if (!$product) {
+                throw new \Exception('Product not found');
+            }
+
+            $orderProduct = $currentOrderProducts->filter(function($op) use($product) {
+                return $op->getProduct()->getId() === $product->getId();
+            })->first();
+
+            if ($orderProduct) {
+                if ($quantity === 0) {
+                    $order->removeOrderProduct($orderProduct);
+                    $this->entityManager->remove($orderProduct);
+                } else {
+                    $orderProduct->setQuantity($quantity);
+                    $orderProduct->setSubtotal($quantity * $product->getPrice());
+                }
+            } else {
+                if ($quantity > 0) {
+                    $newOrderProduct = new OrderProduct();
+                    $newOrderProduct->setOrderEntity($order);
+                    $newOrderProduct->setProductEntity($product);
+                    $newOrderProduct->setQuantity($quantity);
+                    $newOrderProduct->setPricePerUnit($product->getPrice());
+                    $newOrderProduct->setSubtotal($quantity * $product->getPrice());
+
+                    $this->entityManager->persist($newOrderProduct);
+                    $order->addOrderProduct($newOrderProduct);
+                }
+            }
+
+            $totalAmount += $quantity * $product->getPrice();
+        }
+
+        $order->setTotalAmount($totalAmount);
+
+        $deliveryAddress = $order->getAddress() ?: new Address();
+        if (isset($orderAddress['line'])) {
+            $deliveryAddress->setLine($orderAddress['line']);
+            $deliveryAddress->setCity($orderAddress['city']);
+            $deliveryAddress->setCountry($orderAddress['country']);
+            $deliveryAddress->setPostcode($orderAddress['postcode']);
+            $deliveryAddress->setUser($order->getUser());
+            $deliveryAddress->setOrderEntity($order);
+
+            $this->entityManager->persist($deliveryAddress);
+            $order->setAddress($deliveryAddress);
+        }
+
+        $this->entityManager->flush();
+
         return $order;
     }
 
