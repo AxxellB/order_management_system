4c435020da32e3fb9f3ada9903e3015b30a0f1fa
AxxellB <axxellblaze@gmail.com>
2024-10-16 10:31:24 +0300

    Created controller for addresses api

--- backend/src/Controller/AddressController.php
+++ backend/src/Controller/AddressController.php
@@ -1,17 +1,15 @@
 <?php
 
 namespace App\Controller;
-
-use App\Entity\Address;
-use App\Form\AddressFormType;
 use App\Repository\AddressRepository;
 use App\Service\AddressService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api/')]
 class AddressController extends AbstractController
 {
     private AddressService $addressService;
@@ -21,80 +19,212 @@ class AddressController extends AbstractController
         $this->addressService = $addressService;
         $this->addressRepository = $addressRepository;
     }
-    #[Route('/me/addresses', name: 'user_addresses')]
-    public function index(): Response
+
+    #[Route('/addresses', name: 'api_addresses', methods: ['GET'])]
+    public function apiGetAddresses(): JsonResponse
     {
         $user = $this->getUser();
+
+        if (!$user) {
+            return new JsonResponse(['message' => 'You must be logged in to access this page.'], Response::HTTP_UNAUTHORIZED);
+        }
 
         $addresses = array_filter($user->getAddresses()->toArray(), function ($address) {
             return $address->getOrderEntity() == null;
         });
 
-        return $this->render('address/index.html.twig', [
-            'addresses' => $addresses,
+        if(!$addresses){
+            return new JsonResponse([
+                'message' => 'User addresses not found',
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $addressData = array_map(function ($address) {
+            return [
+                'id' => $address->getId(),
+                'line' => $address->getLine(),
+                'line2' => $address->getLine2() ?? "",
+                'city' => $address->getCity(),
+                'postal_code' => $address->getPostCode(),
+            ];
+        }, $addresses);
+
+        $addressData = array_values($addressData);
+
+        return new JsonResponse([
+            'addresses' => $addressData
         ]);
     }
 
-    #[Route('/me/create-address', name: 'create_address', methods: ['GET', 'POST'])]
-    public function createAddress(Request $request): Response
+    #[Route('/address/{id}', name: 'api_get_address', methods: ['GET'])]
+    public function apiGetAddress(int $id): JsonResponse
     {
         $user = $this->getUser();
 
         if (!$user) {
-            throw new AccessDeniedHttpException('You must be logged in to access this page.');
+            return new JsonResponse(['message' => 'You must be logged in to access this page.'], Response::HTTP_UNAUTHORIZED);
         }
 
-        $address = new Address();
-        $form = $this->createForm(AddressFormType::class, $address);
-        $form->handleRequest($request);
+        $address = $this->addressRepository->find($id);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->addressService->addAddress($user, $address);
-
-            $this->addFlash('success', 'You have successfully created a new address.');
-            return $this->redirectToRoute('user_addresses');
+        if(!$address){
+            return new JsonResponse([
+                'message' => 'Address not found',
+            ], Response::HTTP_NOT_FOUND);
         }
 
-        return $this->render('address/createAddress.html.twig', [
-            'form' => $form->createView(),
+        $addressData = [
+                'id' => $address->getId(),
+                'line' => $address->getLine(),
+                'line2' => $address->getLine2() ?? "",
+                'city' => $address->getCity(),
+                'postal_code' => $address->getPostCode(),
+            ];
+
+        return new JsonResponse([
+            'address' => $addressData
         ]);
     }
 
-    #[Route('/me/edit-address/{id}', name: 'edit_address', methods: ['GET', 'POST'])]
-    public function editAddress(Request $request, int $id): Response
+    #[Route('/addresses', name: 'api_create_address', methods: ['POST'])]
+    public function apiCreateAddress(Request $request): JsonResponse
     {
-        $address = $this->addressRepository->find($id);
+        $user = $this->getUser();
 
-        if (!$address) {
-            throw $this->createNotFoundException('Address not found');
+        if (!$user) {
+            return new JsonResponse(['message' => 'You must be logged in to access this page.'], Response::HTTP_UNAUTHORIZED);
         }
 
-        $form = $this->createForm(AddressFormType::class, $address);
-        $form->handleRequest($request);
+        $data = json_decode($request->getContent(), true);
+        $addressResult = $this->addressService->addAddress($user, $data);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->addressService->editAddress($address);
-
-            $this->addFlash('success', 'Address updated successfully.');
-            return $this->redirectToRoute('user_addresses');
-        }
-
-        return $this->render('address/editAddress.html.twig', [
-            'form' => $form->createView(),
+        return new JsonResponse([
+            $addressResult
         ]);
     }
 
-    #[Route('/me/delete-address/{id}', name: 'delete_address', methods: ['POST'])]
-    public function deleteAddress(int $id): Response
+    #[Route('/address/{id}', name: 'api_edit_address', methods: ['PUT'])]
+    public function apiEditAddress(int $id, Request $request): JsonResponse
     {
-        $address = $this->addressRepository->find($id);
+        $user = $this->getUser();
 
-        if (!$address) {
-            throw $this->createNotFoundException('Address not found');
+        if (!$user) {
+            return new JsonResponse(['message' => 'You must be logged in to access this page.'], Response::HTTP_UNAUTHORIZED);
         }
 
-        $this->addressService->removeAddress($address);
-        $this->addFlash('success', 'Address deleted successfully.');
-        return $this->redirectToRoute('user_addresses');
+        $address = $this->addressRepository->find($id);
+
+        if(!$address){
+            return new JsonResponse([
+                'message' => 'Address not found',
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $data = json_decode($request->getContent(), true);
+        $addressResult = $this->addressService->editAddress($data, $address);
+
+        return new JsonResponse([
+            $addressResult
+        ]);
     }
+
+    #[Route('/address/{id}', name: 'api_delete_address', methods: ['DELETE'])]
+    public function apiDeleteAddress(int $id): JsonResponse
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            return new JsonResponse(['message' => 'You must be logged in to access this page.'], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $address = $this->addressRepository->find($id);
+
+        if(!$address){
+            return new JsonResponse([
+                'message' => 'Address not found',
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $this->addressService->deleteAddress($address);
+
+        return new JsonResponse([
+            'message' => 'Address deleted successfully!'
+        ], Response::HTTP_NO_CONTENT);
+    }
+
+//    #[Route('/me/addresses', name: 'user_addresses')]
+//    public function index(): Response
+//    {
+//        $user = $this->getUser();
+//
+//        $addresses = array_filter($user->getAddresses()->toArray(), function ($address) {
+//            return $address->getOrderEntity() == null;
+//        });
+//
+//        return $this->render('address/index.html.twig', [
+//            'addresses' => $addresses,
+//        ]);
+//    }
+
+//    #[Route('/me/create-address', name: 'create_address', methods: ['GET', 'POST'])]
+//    public function createAddress(Request $request): Response
+//    {
+//        $user = $this->getUser();
+//
+//        if (!$user) {
+//            throw new AccessDeniedHttpException('You must be logged in to access this page.');
+//        }
+//
+//        $address = new Address();
+//        $form = $this->createForm(AddressFormType::class, $address);
+//        $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $this->addressService->addAddress($user, $address);
+//
+//            $this->addFlash('success', 'You have successfully created a new address.');
+//            return $this->redirectToRoute('user_addresses');
+//        }
+//
+//        return $this->render('address/createAddress.html.twig', [
+//            'form' => $form->createView(),
+//        ]);
+//    }
+//    #[Route('/me/edit-address/{id}', name: 'edit_address', methods: ['GET', 'POST'])]
+//    public function editAddress(Request $request, int $id): Response
+//    {
+//        $address = $this->addressRepository->find($id);
+//
+//        if (!$address) {
+//            throw $this->createNotFoundException('Address not found');
+//        }
+//
+//        $form = $this->createForm(AddressFormType::class, $address);
+//        $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $this->addressService->editAddress($address);
+//
+//            $this->addFlash('success', 'Address updated successfully.');
+//            return $this->redirectToRoute('user_addresses');
+//        }
+//
+//        return $this->render('address/editAddress.html.twig', [
+//            'form' => $form->createView(),
+//        ]);
+//    }
+//
+//    #[Route('/me/delete-address/{id}', name: 'delete_address', methods: ['POST'])]
+//    public function deleteAddress(int $id): Response
+//    {
+//        $address = $this->addressRepository->find($id);
+//
+//        if (!$address) {
+//            throw $this->createNotFoundException('Address not found');
+//        }
+//
+//        $this->addressService->removeAddress($address);
+//        $this->addFlash('success', 'Address deleted successfully.');
+//        return $this->redirectToRoute('user_addresses');
+//    }
 }
--- backend/src/Service/AddressService.php
+++ backend/src/Service/AddressService.php
@@ -21,13 +21,41 @@ class AddressService
         $this->em->flush();
     }
 
-    public function editAddress(Address $address)
+    public function editAddress(array $data, Address $address)
     {
+        $constraints = new Assert\Collection([
+            'line' => new Assert\NotBlank(['message' => 'Line is required.']),
+            'line2' => new Assert\Optional(),
+            'city' => new Assert\NotBlank(['message' => 'City is required.']),
+            'country' => new Assert\NotBlank(['message' => 'Country is required.']),
+            'postcode' => new Assert\NotBlank(['message' => 'Postal code is required.']),
+        ]);
+
+        $errors = $this->validator->validate($data, $constraints);
+
+        if (count($errors) > 0) {
+            return [
+                'status' => 'error',
+                'errors' => $this->formatValidationErrors($errors)
+            ];
+        }
+
+        $address->setLine($data['line']);
+        $address->setLine2($data['line2'] ?? null);
+        $address->setCity($data['city']);
+        $address->setCountry($data['country']);
+        $address->setPostcode($data['postcode']);
+
         $this->em->persist($address);
         $this->em->flush();
+
+        return [
+            'status' => 'success',
+            'address' => $address,
+        ];
     }
 
-    public function removeAddress(Address $address)
+    public function deleteAddress(Address $address)
     {
         $this->em->remove($address);
         $this->em->flush();
