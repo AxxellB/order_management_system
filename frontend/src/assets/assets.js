c3d62322aa87be08be6399f412c2cc2529ccca84
Joro Protopopov <gopetoo98@gmail.com>
2024-11-04 14:53:16 +0200

    Changed Website logo image

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,9 +1,7 @@
 import ExampleCsvFormat from "./imgs/example_csv_bulk_restock.png";
-import Logo from "./imgs/_.jpeg";
-import LogoBG from "./imgs/removed_background_logo.png";
+import LogoBG from "./imgs/worldwide.png";
 
 export {
     ExampleCsvFormat,
-    Logo,
     LogoBG,
 };
